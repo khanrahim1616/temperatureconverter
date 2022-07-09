f7b6d9e4206fb94548de39316c2bbc73740f0a58
khanrahim1616 <khanrahim1616@gmail.com>
2022-07-09 21:22:28 +0500

    changes

---
+++ script.js
@@ -0,0 +1,29 @@
+const calculatetemp=()=>{
+    
+    const numbertemp=document.getElementById('temp').value;
+    const tempselected=document.getElementById('temp_diff');
+    const  valuetemp=temp_diff.options[tempselected.selectedIndex].value;
+
+    const celtofah=(cel)=>{
+        let Fahrenheit=((cel*9/5)+32)
+        return Fahrenheit;
+    }
+   
+    const fahtocel=(fehr)=>{
+        let celcius=((fehr-32)*5/9)
+        return celcius;
+    }
+   
+    let result;
+
+    if(valuetemp=='cel'){
+        result=celtofah(numbertemp);
+        document.getElementById('resultcontainer').innerHTML =`=${result}°Fahrenheit`;
+    }else{
+        result=fahtocel(numbertemp);
+        document.getElementById('resultcontainer').innerHTML =`=${result}°Celsius`;
+    }
+
+    
+}
+// calculatetemp()  
